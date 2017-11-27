22912c1e9e771bb5c56de1f6759e42c93a3bb818
mac load <macload1@yahoo.de>
2017-11-27 20:40:33 +0100

    mod

--- python/examples/sst25vf016b.py
+++ python/examples/sst25vf016b.py
@@ -1,12 +1,12 @@
 import spidev
 
-#~ spi.open(0,0)	            Öffnet den SPI-Bus 0 mit CS0
-#~ spi.open(0,1)	            Öffnet den SPI-Bus 0 mit CS1
-#~ spi.close()	                Schliesst den SPI-Bus
-#~ spi.readbytes(len)	        Liest len Bytes vom SPI-Slave
-#~ spi.writebytes([array of bytes])	Sendet ein Byte-Array zum SPI-Slave
-#~ spi.xfer([array of bytes])	Sendet ein Byte-Array, CEx wird vor jedem Byte aktiv und dann wieder inaktiv
-#~ spi.xfer2([array of bytes])	Sendet ein Byte-Array, dabei bleibt CEx dauerhaft aktiv
+# spi.open(0,0)	                Öffnet den SPI-Bus 0 mit CS0
+# spi.open(0,1)	                Öffnet den SPI-Bus 0 mit CS1
+# spi.close()	                Schliesst den SPI-Bus
+# spi.readbytes(len)	        Liest len Bytes vom SPI-Slave
+# spi.writebytes([array of bytes])	Sendet ein Byte-Array zum SPI-Slave
+# spi.xfer([array of bytes])	Sendet ein Byte-Array, CEx wird vor jedem Byte aktiv und dann wieder inaktiv
+# spi.xfer2([array of bytes])	Sendet ein Byte-Array, dabei bleibt CEx dauerhaft aktiv
 
 # SPI transfer: 1 byte instruction + 3 bytes address + n bytes write or read
 
